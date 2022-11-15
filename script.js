bcb093966f81bd6215b5246715105ca1ca5eac61
Vadym <vadymsenior@gmail.com>
2022-11-15 17:36:16 +0200

    fix inversion in mac and add animation on accordion

--- script.js
+++ script.js
@@ -82,16 +82,20 @@ $(document).ready(function () {
       };
     });
 
-    window.addEventListener("scroll", () => {
-      const setColor = offsetSections
-        .filter((item) => window.scrollY >= item.offset - deltaHeight)
-        .at(-1).color;
-      const classListClear = ["blue", "white", "dark"].filter(
-        (item) => item !== setColor
-      );
-      header.classList.remove(...classListClear);
-      !header.classList.contains(setColor) && header.classList.add(setColor);
-    });
+    const multipleEventListener = (event) => {
+      window.addEventListener(event, () => {
+        const setColor = offsetSections
+          .filter((item) => window.scrollY >= item.offset - deltaHeight)
+          .at(-1).color;
+        const classListClear = ["blue", "white", "dark"].filter(
+          (item) => item !== setColor
+        );
+        header.classList.remove(...classListClear);
+        !header.classList.contains(setColor) && header.classList.add(setColor);
+      });
+    };
+    multipleEventListener("scroll");
+    multipleEventListener("load");
   };
 
   listenChangeColor();
