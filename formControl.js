b9867481cd0a23091fa2b07ab88490e26ba4f31d
SlavaPachevsky <savaukr17101980@gmail.com>
2022-11-21 11:37:15 +0200

    debounce 7c

--- formControl.js
+++ formControl.js
@@ -221,7 +221,7 @@ const formAction = (parent, successForm) => {
     }
   }
 
-  const debounceSendPostData = debounce(sendPostData, 6000);
+  const debounceSendPostData = debounce(sendPostData, 7000);
 
   const select = form.querySelector(".select-country");
   select.addEventListener("change", () => {
